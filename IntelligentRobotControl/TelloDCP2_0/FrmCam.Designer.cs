3c8cb7e45077e7c088e6a967cedc3443e10d7e3a
mjkim0819 <minails@naver.com>
2023-10-03 23:26:29 +0900

    TelloDCP version up(ver.2) FrmCam is show tello's view with tello cam.

---
+++ IntelligentRobotControl/TelloDCP2_0/FrmCam.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace TelloDCP2_0
+{
+    partial class FrmCam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbData = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 373);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(682, 30);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnStart.Location = new System.Drawing.Point(0, 0);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 30);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnStop.Location = new System.Drawing.Point(607, 0);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 30);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lbData
+            // 
+            this.lbData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbData.FormattingEnabled = true;
+            this.lbData.ItemHeight = 15;
+            this.lbData.Location = new System.Drawing.Point(0, 0);
+            this.lbData.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lbData.Name = "lbData";
+            this.lbData.Size = new System.Drawing.Size(682, 403);
+            this.lbData.TabIndex = 2;
+            // 
+            // FrmCam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 403);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbData);
+            this.Name = "FrmCam";
+            this.Text = "FrmCam";
+            this.Load += new System.EventHandler(this.FrmCam_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lbData;
+    }
+}
---
+++ IntelligentRobotControl/TelloDCP2_0/FrmDCP.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelloDCP2_0
+{
+    public partial class FrmDCP : Form
+    {
+        static FrmDCP frmCli;
+        static UdpClient udpCli, udpSvr;
+        static FrmCam wCam;
+        static Thread tUDPServer;
+        public bool tBusy;
+
+        public FrmDCP()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            frmCli = this;
+        }
+        public void severThread()
+        {
+            udpSvr = new UdpClient(8890);
+            lbStatus.Items.Add("Waiting for Tello status...");
+
+            while (tBusy)
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+                Byte[] rb = udpSvr.Receive(ref ep);
+                string status = BitConverter.ToString(rb);
+                string rd = Encoding.ASCII.GetString(rb);
+
+                lbStatus.Items.Add(ep.ToString() + ": " + rd);
+            }
+            lbStatus.Items.Add("Sever disconnected.");
+            udpSvr.Close();
+        }
+        public static void ReceiveCallback(IAsyncResult ar)
+        {
+            UdpClient sock = ar.AsyncState as UdpClient;
+            IPEndPoint source = new IPEndPoint(IPAddress.Any, 0);
+            byte[] message = sock.EndReceive(ar, ref source);
+            string receiveString = Encoding.ASCII.GetString(message);
+
+            frmCli.lbComm.Items.Add(source.ToString() + ": " + receiveString);
+            sock.BeginReceive(new AsyncCallback(ReceiveCallback), sock);
+        }
+        public void udpcommand(string msg)
+        {
+            udpCli.BeginReceive(new AsyncCallback(ReceiveCallback), udpCli);
+
+            IPEndPoint target = new IPEndPoint(IPAddress.Parse("192.168.10.1"), 8889);
+            Byte[] senddata = Encoding.ASCII.GetBytes(msg);
+
+            frmCli.lbComm.Items.Add(">>" + msg);
+            udpCli.Send(senddata, senddata.Length, target);
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            udpcommand(cbCommand.Text);
+        }
+
+        private void btnTakeoff_Click(object sender, EventArgs e)
+        {
+            udpcommand("takeoff");
+        }
+        private void btnLand_Click(object sender, EventArgs e)
+        {
+            udpcommand("land");
+        }
+        private void FrmDCP_Load(object sender, EventArgs e)
+        {
+            frmCli = this;
+            udpCli = new UdpClient(8889);
+            tUDPServer = new Thread(new ThreadStart(severThread));
+            tBusy = true;
+            tUDPServer.Start();
+        }
+        private void btnCam_Click(object sender, EventArgs e)
+        {
+            wCam = new FrmCam(this);
+            wCam.Show();
+            wCam.Location = new Point(this.Right, this.Top);
+        }
+    }
+}
