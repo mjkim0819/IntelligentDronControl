d906ebc38a20845b84a7485f3a74b748047a6dbe
mjkim0819 <minails@naver.com>
2023-10-07 00:34:46 +0900

    when start FrmFp, draw a grid. but it still too small size

--- IntelligentRobotControl/TelloDCP3_0/FrmFp.Designer.cs
+++ IntelligentRobotControl/TelloDCP3_0/FrmFp.Designer.cs
@@ -144,6 +144,7 @@ namespace TelloDCP3_0
             this.Controls.Add(this.pbRoom);
             this.Name = "FrmFp";
             this.Text = "Flight Plan";
+            this.Load += new System.EventHandler(this.FrmFp_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tbGrid)).EndInit();
